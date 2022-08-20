577b5a94c54b97542165c70527dbfa818ba16e08
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-08-20 18:06:45 +1200

    Add image grid for desktop

--- client/components/Regions.jsx
+++ client/components/Regions.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-
+import styles from '../styles/regions.module.scss'
 import { fetchRegions } from '../actions'
 
 function Regions() {
@@ -12,25 +12,27 @@ function Regions() {
   }, [])
 
   return (
-    <div>
-      <div className="regions-container row row-cols-2 ">
+    
+      <div className={styles.container}>
         {regions?.map((region) => (
-          <div
-            className="region-tile col-6 col-md-4"
+          <ul
+            className={styles.regionImgGrid}
             key={region.id}
             src={region.region_name}
           >
+            <li>
             <Link to={`/region/${region.region_name}/${region.id}`}>
               <img
-                src="https://images.unsplash.com/photo-1656926207579-aae7089631c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                src={region.image}
                 className="img-thumbnail"
                 alt="..."
               />
             </Link>
-          </div>
+            </li>
+          </ul>
         ))}
       </div>
-    </div>
+    
   )
 }
 
