5d97986bda7c65eaf6db816aeef79a80aa1ace81
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-08-25 13:02:12 +1200

    Add boxshadow and div margins

--- client/components/Locations.jsx
+++ client/components/Locations.jsx
@@ -25,23 +25,24 @@ function Locations() {
           {locationArr?.map((location) => (
             <li key={location.id}>
               <img src={location.image} alt="" />
-              <h2>{location.name}</h2>
+              <div><h2>{location.name}</h2></div>
               <div><h4>About:</h4>{location.description}</div>
               <div><h4>Location:</h4>{location.address}</div>
               <div><h4>Opening Hours:</h4>{location.openingHours}</div>
               <div><h4>Website:</h4><a href={location.websiteUrl} target="_blank" rel="noreferrer">
                  aucklandmuseum.com
               </a></div>
-  
-              <h4>Facilities:</h4>
-              {location.wheelchairCompatible == 1 ? <p>Wheelchair Accessible: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Wheelchair Accessibility: <span className={styles.cross} alt="No">&#10005;</span></p>}
 
-              {location.ramps == 1 ? <p>Ramps: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Ramps: <span className={styles.cross} alt="No">&#10005;</span></p>}
+            <div>
+                <h4>Facilities:</h4>
+                {location.wheelchairCompatible == 1 ? <p>Wheelchair Accessible: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Wheelchair Accessibility: <span className={styles.cross} alt="No">&#10005;</span></p>}
 
-              {location.elevator == 1 ? <p>Elevator: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Elevator: <span className={styles.cross} alt="No">&#10005;</span></p>}
-             
-              {location.accessibleToilets == 1 ? <p>Accessible Toilets: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Accessible Toilets: <span className={styles.cross} alt="No">&#10005;</span></p>}
+                {location.ramps == 1 ? <p>Ramps: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Ramps: <span className={styles.cross} alt="No">&#10005;</span></p>}
+
+                {location.elevator == 1 ? <p>Elevator: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Elevator: <span className={styles.cross} alt="No">&#10005;</span></p>}
               
+                {location.accessibleToilets == 1 ? <p>Accessible Toilets: <span className={styles.tick} alt="Yes">&#10003;</span></p> : <p>Accessible Toilets: <span className={styles.cross} alt="No">&#10005;</span></p>}
+              </div>
             </li>
           ))}
         </ul>
