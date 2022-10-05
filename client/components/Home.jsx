774188ae82fddc30d8551e4c49d3e52c4a7e4384
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-10-05 15:37:35 +1300

    Add welcome card

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,14 +9,23 @@ import styles from '../styles/home.module.scss'
 export default function Home() {
 
   return (
-<div>
-  <div >
-   <img className={styles.backgroundImg} src="/images/homebackground.jpg" alt="" />
+<div className={styles.container}>
+  <div>
+    <div className={styles.intro}>
+      <h2>Accessible Auckland</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur voluptate adipisci repellat et, sunt, autem ex asperiores eaque sit voluptates nemo similique unde ratione cumque! Doloremque expedita nostrum similique soluta.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur voluptate adipisci repellat et, sunt, autem ex asperiores eaque sit voluptates nemo similique unde ratione cumque! Doloremque expedita nostrum similique soluta.</p>
+      <button>Get Started</button>
+    </div>
+  
 
       {/* <Carousel />
       <Regions /> */}
     
   </div>
+  <div className={styles.imageContainer}>
+      <img src="/images/homebackground.jpg" alt="" />
+    </div>
 </div>
   )
 }
