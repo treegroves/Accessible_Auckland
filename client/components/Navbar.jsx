ce5aa37380f6107eb6fa9c38e35d734a83415dad
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-10-05 13:32:29 +1300

    Add image and style nav

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -5,6 +5,7 @@ import styles from "../styles/navbar.module.scss"
 
 export default function Navbar() {
   return (
+
     <nav className={styles.navContainer}>
       <h1 className={styles.logo}>Accessible Auckland</h1>
       <div className={styles.links}>
