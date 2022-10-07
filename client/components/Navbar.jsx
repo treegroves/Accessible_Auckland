173c43a0322b517cb21ced8e1afd4d63954473f9
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-10-07 14:58:55 +1300

    Add search based on url

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,17 +1,41 @@
 
-import React from "react"
-import { Link } from "react-router-dom"
+import React, {useState} from "react"
+import { Link, useNavigate } from "react-router-dom"
 import styles from "../styles/navbar.module.scss"
+import { useDispatch } from "react-redux"
+import { searchLocations } from '../actions'
 
 export default function Navbar() {
+const navigate = useNavigate()
+
+const [search, setSearch] = useState('')
+const dispatch = useDispatch()
+function handleSubmit(event) {
+  event.preventDefault()
+  dispatch(searchLocations(search))
+  setSearch('')
+  navigate(`/region/${location.regionId}/${location.id}/${search}`)
+}
+
   return (
-  
     <nav className={styles.navContainer}>
       <h1 className={styles.logo}>Accessible Auckland</h1>
       <div className={styles.links}>
         <Link to="/">Home</Link>
         <Link to="/region">Regions</Link>
         <Link to="#">About</Link>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            placeholder="Search"
+            name = "search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value) } />
+
+          <button></button>
+        </form>
       </div>
     </nav>
   )
